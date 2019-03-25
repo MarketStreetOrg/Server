cb7b7b8697b69a1bb45cc2f100e8dafbc259ec99
StevenKatabalwa <StevenKatabalwa2019@gmail.com>
2019-03-24 21:04:09 -0500

    Mongo Integration

---
+++ Katale-Server/Katale-Server-Final/Database/Source/LocalMongoDBConfig.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Katale_Server_Final.Database.Source
+{
+    public class LocalMongoDBConfig : IConfig
+    {
+        public string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString.ToString();
+        }
+
+        public void LoadConfigurations()
+        {
+           
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Katale-Server/Katale-Server-FinalTests/Database/DAO/DepartmentMongoTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Katale_Server_.Models;
+using Katale_Server_Final.Database.Mongo;
+using Katale_Server_Final.Database.Mongo.Implementation;
+using Katale_Server_Final.Service;
+using Katale_Server_Final.Service.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Katale_Server_FinalTests.Database.DAO
+{
+    [TestClass]
+    public class DepartmentMongoTests
+    {
+
+        static IMongoDAO<Department> mongoDAO =new DepartmentMongoDAO();
+        IDepartmentService departmentService = new DepartmentService(mongoDAO);
+
+        [TestMethod]
+        public void TestSaveDepartment()
+        {
+            Department department = new Department("Test Mongo Department", "This is my mongo department");
+
+            mongoDAO.Save(department);
+
+            departmentService.GetAll().ForEach(dpt =>
+            {
+                mongoDAO.Save(dpt);
+            });
+
+        }
+
+        [TestMethod]
+        public void TestUpdateDepartment()
+        {   
+            Department department = new Department("Test for Mongo Department", "This is my Service updated mongo department");
+       
+            departmentService.Update(department);
+
+        }
+
+        [TestMethod]
+        public void TestGetAllDepartments()
+        {
+            List<Department> departments = departmentService.GetAll();
+
+        }
+
+        [TestMethod]
+        public void TestDeleteDepartment()
+        {
+            departmentService.Delete(0);
+        }
+
+        [TestMethod]
+        public void TestGetSingleDepartment()
+        {
+            departmentService.GetSingle("Groceries");
+        }
+    }
+}
